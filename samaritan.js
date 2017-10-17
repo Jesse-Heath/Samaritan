9052d779f986a698508382d5b7258d7352e21188
Jesse Heath <jesse.heath@comotion.co.za>
2017-10-17 08:26:02 +0200

    Added check before purge if you want to or not

--- samaritan.js
+++ samaritan.js
@@ -49,7 +49,58 @@ client.on("message", (message) => {
                     });
                 break;
             case "purge":
-                purgeChannel(message);
+                message.channel.send('Are you sure? Answer yes or no')
+                .then(sentMessage => {
+                    message.channel.awaitMessages(response => (response.content === 'yes' || response.content === 'no'), {
+                        max: 1,
+                        time: 30000,
+                        errors: ['time'],
+                    })
+                    .then((collected) => {
+                        if (collected.first().content.toLowerCase() === "yes") {
+                            message.channel.send(`Ok, purging channel...`)
+                            .then(sentMessage2 => {
+                                setTimeout(function() {
+                                    sentMessage2.delete();
+                                    collected.first().delete();
+                                    sentMessage.delete();
+                                    message.delete();
+                                    purgeChannel(message);
+                                }, 5000);
+                            });
+                        } else if (collected.first().content.toLowerCase() === "no") {
+                            message.channel.send(`Next time be sure before you ask me ${config.emoji.angry}`)
+                            .then(sentMessage2 => {
+                                setTimeout(function() {
+                                    sentMessage2.delete();
+                                    collected.first().delete();
+                                    sentMessage.delete();
+                                    message.delete();
+                                }, 5000);
+                            });
+                        } else {
+                            message.channel.send(`Unknown response ${config.emoji.shrug}`)
+                            .then(sentMessage2 => {
+                                setTimeout(function() {
+                                    sentMessage2.delete();
+                                    collected.first().delete();
+                                    sentMessage.delete();
+                                    message.delete();
+                                }, 5000);
+                            });
+                        }
+                    })
+                    .catch(() => {
+                        message.channel.send(`You took to long! ${config.emoji.shrug} I\'m not doing it any more`)
+                        .then(sentMessage2 => {
+                            setTimeout(function() {
+                                message.delete();
+                                sentMessage.delete();
+                                sentMessage2.delete();
+                            }, 5000);
+                        });
+                    });
+                });
                 break;
             case "remind":
                 console.log("remind command triggered");
